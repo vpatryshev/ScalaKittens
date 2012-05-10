1d9e73d8579361a35e7bb6ef7b76b341cc2ff2a3
Vlad Patryshev <vpatryshev@gmail.com>
2012-05-09 23:49:40 -0700

    20000 threads in a test case; all comments done.

--- src/main/scala/scalakittens/Caching.scala
+++ src/main/scala/scalakittens/Caching.scala
@@ -2,31 +2,68 @@ package scalakittens
 
 import java.util.concurrent.atomic.AtomicReference
 import java.util.concurrent.TimeUnit
+import scalakittens.Caching.CacheUnit
 
 /**
+ * This trait/object's purpose is to be used for caching:
+ * - individual instances
+ * - which are hard to get (takes time)
+ * - which are valid for a limited amount of time, and can be retrieved again
+ * - which are not a burden to store in memory, say, in a map, say, in a ConcurrentHashMap
+ *
+ * Use something else if you have different requriements.
+ * Or just let me know; this is work in progress. We had a bunch of caching stuff at Google, seems like now Google Guava
+ * has cache package added. But that's Java, the thing of the past.
+ *
+ * The trick here is this: I use both AtomicReference and Scala's lazy val to store an expiring value and retrieve it only
+ * once and only on demand.
+ *
+ * The algorithm is this. A class named Container stores the following data:
+ * - a function that retrieves the value
+ * - timestamp of a moment a new instance of CacheUnit was created (nanos)
+ * - a lazy value that is cached; it is evaluated when the value is being retrieved, and Scala's lazy val mechanism
+ *   (based on 'synchronized'), ensures that only one instance is evaluated.
+ * An atomic reference to this Container is stored in an instance of CacheUnit; CacheUnit is the entity returned when
+ * you create it, and it is immutable (with caveats).
+ *
+ * CacheUnit's apply() method returns the fresh value; if the value stored in current Container is not fresh, it's reevaluated.
+ *
+ * You can find use cases in the test. Here's one:
+ * <code>
+ *    var counter: Char = 'a'
+ *    val cached = sut.cache(() => { counter = (counter+1).toChar; counter}) validFor 10 HOURS
+ *</code>
+ * We declare a cache that invalidates after 10 hours and retrieves a counter value, with a side-effect of bumping the counter.
  */
-
 trait Caching {
 
-  protected def now: Long
+  // indirection that is good for applying cake pattern: see the object below and the test where time is mocked
+  protected def now: Long // expecting nanos
 
-  class CacheUnit[T](fun:() => T, timeout: Long) {
+  class CacheUnit[T](fun:() => T, timeout_nano: Long) {
 
     private class Container(validUntil_nano: Long) {
       lazy val value: T = fun()
       def stillValid = now < validUntil_nano
     }
 
-    private def update = new Container(now + timeout)
+    private def newContainer = new Container(now + timeout_nano)
  
-    private val ref = new AtomicReference[Container](update)
+    private val ref = new AtomicReference[Container](newContainer)
 
     def apply(): T = {
-      val current = ref.get
-      current.stillValid || ref.compareAndSet(current, update) // avoiding multiple resettings
+      val current = ref.get 
+      current.stillValid || ref.compareAndSet(current, newContainer) // avoiding multiple resettings
       ref.get.value
     }
-    
+
+    def withTimeout(timeout: Long, unit: TimeUnit): CacheUnit[T] = new CacheUnit[T](fun, unit toNanos timeout)
+
+    /**
+     * Specify the time it is valid for, with units
+     * @param timeout units are specified further down
+     * @return a half-way object that needs a time unit to produce a cache unit (hope you follow me)
+     */
     def validFor(timeout: Long) = new {
       def NANOSECONDS  = apply(TimeUnit.NANOSECONDS)
       def MICROSECONDS = apply(TimeUnit.MICROSECONDS)
@@ -35,15 +72,15 @@ trait Caching {
       def MINUTES      = apply(TimeUnit.MINUTES)
       def HOURS        = apply(TimeUnit.HOURS)
       def DAYS         = apply(TimeUnit.DAYS)
-      def apply(unit: TimeUnit) = new CacheUnit[T](fun, unit toNanos timeout)
+      private def apply(unit: TimeUnit) = withTimeout(timeout, unit)
     }
   }
 
-  def cache[T](fun: () => T) = new CacheUnit[T](fun, Long.MaxValue)
+  def cache[T](fun: () => T) = new CacheUnit[T](fun, Long.MaxValue) // by default it's valid forever
 }
 
 object Caching extends Caching {
   protected def now = System.nanoTime
   
-  implicit def currentValue[T](cu: CacheUnit[T]): T = cu.apply
+  implicit def currentValue[T](cu: CacheUnit[T]): T = cu.apply()
 }
--- src/test/scala/scalakittens/CachingTest.scala
+++ src/test/scala/scalakittens/CachingTest.scala
@@ -14,13 +14,13 @@ object CachingTest extends Specification {
   var myTime: Long = 0
 
   val sut = new Caching {
-    def now = myTime
+    def now = myTime // using cake pattern to mock time
   }
-  implicit def currentValue[T](cu: CacheUnit[T]): T = cu.apply
+  implicit def currentValue[T](cu: CacheUnit[T]): T = cu.apply()
 
   "A Cache" should {
     "return the right stuff" in {
-      var i: Int = 0
+      var i: Int = 0 // boring
       val cached = sut.cache(() => {i}).validFor(3).NANOSECONDS
       i = 17
       val n1: Int = cached()
@@ -28,46 +28,48 @@ object CachingTest extends Specification {
       i = 18
       val n2: Int = cached()
       n2 must_== 17
-      myTime = 2
+      myTime = 2 // still valid eh
       val n3: Int = cached()
       n3 must_== 17
-      myTime = 3
+      myTime = 3 // now it's stale, and will update
       val n4: Int = cached()
-      n4 must_== 18
+      n4 must_== 18 // yesss!!!
       i = 19
       myTime = 10
-      val n5: Int = cached()
+      val n5: Int = cached() // one more time, to make sure it's not stuck
       n5 must_== 19
     }
 
     "only call function once per period" in {
-      val mutex = new Object
-      def pause = mutex.synchronized(mutex.wait)
-      def signal = mutex.synchronized(mutex.notifyAll)
+      val mutex = new Object // used in controlling the 20k threads
+      def pause  { mutex.synchronized(mutex.wait) }
+      def signal { mutex.synchronized(mutex.notifyAll) }
       val numTries = 15
 
       var counter: Char = 'a'
       val cached = sut.cache(() => { counter = (counter+1).toChar; counter}) validFor 10 HOURS
 
       def startConsuming(out: Writer) = new Thread {
-        override def run {
-          (1 to numTries) foreach ( (i:Int) => { out.write(cached()); pause } )
+        override def run { // this thread will
+          (1 to numTries) foreach ( // repeat this number of times
+            (i:Int) => { out.write(cached()) // log the current entry of cache
+                         pause } ) // and wait for a signal
         }
-      }.start
+      }.start // yeah, launch it
 
-      val buffers = ((1 to 2000) map ((i:Int) => new StringWriter)).toList
-      val TEN_HOURS = TimeUnit.HOURS toNanos 10
+      val buffers = ((1 to 20000) map ((i:Int) => new StringWriter)).toList // have so many buffers
+      val TEN_HOURS = TimeUnit.HOURS toNanos 10 // an arbitrary timeout value
 
-      buffers foreach startConsuming
+      buffers foreach startConsuming // right, we start so many threads, one per buffer
 
-      Thread sleep 20
+      Thread sleep 200 // give them all a chance to reach the gate
 
-      for (i <- 1 to numTries) {
-        myTime = myTime + TEN_HOURS + 1
-        signal
-        Thread sleep 100
+      for (i <- 1 to numTries) { // now control our threads, they are waiting
+        myTime = myTime + TEN_HOURS + 1 // time's up for a value
+        signal // tell them all to consume - only one is supposed to bump the counter
+        Thread sleep 1000 // give them a chance to do their job
       }
-      buffers foreach (_.toString must_== "bcdefghijklmnop")
+      buffers foreach (_.toString must_== "bcdefghijklmnop") // this is the ideal we are looking for
     }
   }
 }
