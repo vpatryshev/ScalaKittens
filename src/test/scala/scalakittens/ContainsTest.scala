fa80f5a8daabf8c89a82c73c4ec8780785aed58c
vpatryshev <vpatryshev@gmail.com>
2013-08-03 20:52:18 -0700

    This is work in progress (may not even compile); will fix it on another machine).

--- src/test/scala/scalakittens/ContainsTest.scala
+++ src/test/scala/scalakittens/ContainsTest.scala
@@ -46,12 +46,21 @@ object ContainsTest extends Specification {
       val listA = A("3") :: listB
       listA.size must_== 3
       listA contains A("3") must beTrue
-      listA contains B("1") must beTrue
+      listA contains B("1") must beFalse
 //      listA contains "nothingness" must not compile
 //      listB contains A("3") must not compile
+      val cA: Container[A] = listA
+      val cB: Container[B] = listB
+      val cba: Container[B] = cA
+      cA contains B("3") must beTrue
+      cba contains B("3") must beFalse
       val listC: MyList[Any] = listA
       listC contains "abracadabra" must beFalse
       listC contains B("2") must beTrue
+      type F[-A] = Function1[A, String]
+      val m1:F[Object] = Map("abc" -> "ABC")
+      val m2:F[String] = Map("xyz" -> "XYZ")
+      val m3:F[String] = m1
     }
   }
 }
