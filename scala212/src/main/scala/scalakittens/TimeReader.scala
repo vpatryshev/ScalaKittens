e5c59d1db0d1e005c1d87b4f11c49ca72252c105
Vlad Patryshev <vpatryshev@gmail.com>
2017-03-05 10:37:31 -0800

    Copypasted 211 to 212, before working on readaptation.

---
+++ scala212/src/main/scala/scalakittens/TimeReader.scala
@@ -0,0 +1,12 @@
+package scalakittens
+
+/**
+  * Created by vpatryshev on 6/19/16.
+  */
+trait TimeReader {
+  def currentTime: Long
+}
+
+object DateAndTime extends TimeReader {
+  def currentTime = System.currentTimeMillis()
+}
---
+++ scala212/src/main/scala/scalakittens/experiments/algebra/Semigroup.scala
@@ -0,0 +1,14 @@
+package scalakittens.experiments.algebra
+
+import scala.language.{higherKinds, implicitConversions}
+
+trait Semigroup[X] {
+  def add(x: X, y: X): X
+
+  case class Value[A](value: X) {
+    def +(another: X): X = add(value, another)
+  }
+
+  implicit def value[A](x: X): Value[A] = Value(x)
+
+}
